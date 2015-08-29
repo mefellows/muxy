be30a5b8ff9c200043f78b3331bdda4eee001017
Matt Fellows <matt.fellows@onegeek.com.au>
2015-08-29 23:13:32 +1000

    Updating Homebrew formula

--- scripts/muxy.rb
+++ scripts/muxy.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
-class PackerWindowsPlugins < Formula
+class Muxy < Formula
   homepage "https://github.com/packer-community/packer-windows-plugins"
-  version "1.0.0"
+  version "0.0.1"
 
   if Hardware.is_64_bit?
-    url "https://github.com/mefellows/muxy/releases/download/v0.0.1/darwin_amd64.zip"
+    url "https://github.com/mefellows/muxy/releases/download/v#{version}/darwin_amd64.zip"
     sha1 "e09e2ab6f7fc39237b5b41f53aa5b2a815428cfc"
   else
-    url "https://github.com/packer-community/packer-windows-plugins/releases/download/v0.0.1/darwin_386.zip"
+    url "https://github.com/mefellows/muxy/releases/download/v#{version}/darwin_386.zip"
     sha1 '64535683e7f261091629c5a96236263dc0856c63'
   end
 
