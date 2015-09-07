9dd99b93ab1c715e1d317fb0eaff7c7021ca2d3c
Matt Fellows <matt.fellows@onegeek.com.au>
2015-09-07 14:26:12 +1000

    Lightweight logger

---
+++ muxy/log_test.go
@@ -0,0 +1,54 @@
+package muxy
+
+import (
+	"log"
+	"testing"
+)
+
+type FakeWriter struct {
+	Count int
+}
+
+func (w *FakeWriter) Write(p []byte) (n int, err error) {
+	w.Count = w.Count + 1
+	return 0, nil
+}
+
+func TestLogLevel(t *testing.T) {
+
+	writer := &FakeWriter{}
+	logger := &MuxyLogger{Level: DEBUG}
+	log.SetOutput(writer)
+	logger.Log(INFO, "Info")
+	logger.Log(TRACE, "Trace") // Should not appear
+	if writer.Count != 1 {
+		t.Fatalf("Logger should have written once, but received %d calls", writer.Count)
+	}
+	logger.Debug("Debug")
+
+	if writer.Count != 2 {
+		t.Fatalf("Logger should have written twice, only received %d calls", writer.Count)
+	}
+
+	writer.Count = 0
+	logger.Trace("yo")
+	logger.Debug("yo")
+	logger.Info("yo")
+	logger.Error("yo")
+	logger.Warn("yo")
+	if writer.Count != 4 {
+		t.Fatalf("Logger should have written 4 times, received %d calls", writer.Count)
+	}
+
+	logger.Level = TRACE
+	writer.Count = 0
+	logger.Trace("yo")
+	if writer.Count != 1 {
+		t.Fatalf("Logger should have written once, received %d calls", writer.Count)
+	}
+}
+func TestLogColour(t *testing.T) {
+	logger := &MuxyLogger{Level: DEBUG}
+	logger.Log(INFO, "Info %s", logger.Colorize(LIGHTRED, " some words "))
+	logger.Log(INFO, "Info something else not in colour")
+}
