0334431c219e74e19de8236771545d64574c51f5
Matt Fellows <matt.fellows@onegeek.com.au>
2015-09-23 09:43:39 +1000

    Updated test script to exclude non package directories

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-go test -race -v ./...
- 
+go test -v --race $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d | grep -v examples | grep -v pkg | grep -v bin | grep -v scripts  | xargs -I{} echo "{}/")
+
 # Get Test dependencies
 go get github.com/axw/gocov/gocov
 go get github.com/mattn/goveralls
@@ -8,9 +8,8 @@ go get golang.org/x/tools/cmd/cover
 go get github.com/modocache/gover
 
 # Run test coverage on each subdirectories and merge the coverage profile.
- 
 echo "mode: count" > profile.cov
- 
+
 # Standard go tooling behavior is to ignore dirs with leading underscors
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d); do
   if ls $dir/*.go &> /dev/null; then
@@ -21,5 +20,5 @@ for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d)
     fi
   fi
 done
- 
+
 go tool cover -func profile.cov
