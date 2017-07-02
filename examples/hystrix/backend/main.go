bb9a3f49c971bde3274dba49f0e4235b873e7b24
Matt Fellows <matt.fellows@onegeek.com.au>
2017-07-02 13:41:43 +1000

    wip: removed statsd + hystrix client/instrumentation

--- examples/hystrix/backend/main.go
+++ examples/hystrix/backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/quipo/statsd"
 	"github.com/zenazn/goji"
@@ -13,25 +12,11 @@ import (
 var s *statsd.StatsdClient
 
 func ping(c web.C, w http.ResponseWriter, r *http.Request) {
-	s.Incr("backup", 1)
 	fmt.Fprintf(w, "Hello from real API")
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	// Setup Statsd client
-	fmt.Println("Connecting to Statsd server...")
-
-	prefix := "muxy."
-	statsdclient := statsd.NewStatsdClient(os.Getenv("STATSD_HOST"), prefix)
-	s = statsdclient
-	statsdclient.CreateSocket()
-
-	if s == nil {
-		fmt.Println("Could not connect to statsd server, exiting")
-		os.Exit(1)
-	}
-
 	goji.Get("/*", ping)
 	goji.Serve()
 }
